a7b6ec52696d53b6ec943a46faee2866b57305d0
bucketofsandy <sander.potters@xs4all.nl>
2021-04-12 19:44:47 +0200

    plankmaker fix

--- plankmaker/src/main/java/net/runelite/client/plugins/plankmaker/plankmakerPlugin.java
+++ plankmaker/src/main/java/net/runelite/client/plugins/plankmaker/plankmakerPlugin.java
@@ -388,7 +388,7 @@ public class plankmakerPlugin extends Plugin
 	}
 
 	private void makePlank() {
-		targetMenu = new MenuEntry("Make", "<col=ff9040>Oak - 250gp</col>", 1, 57, -1, 17694735, false);
+		targetMenu = new MenuEntry("Make", "<col=ff9040>Oak - 250gp</col>", 1, 57, -1, 17694736, false);
 		mouse.delayMouseClick(client.getWidget(270,15).getBounds(), sleepDelay());
 		}
 
